41ba19e9cbead017502efaeb7dd0f9cfb91bfb5a
dldbfl <pangpang1861@naver.com>
2020-04-16 21:04:00 +0900

    first commit

--- JSP_AdminLTE3/src/com/jsp/exception/NotFoundIDException.java
+++
@@ -1,9 +0,0 @@
-package com.jsp.exception;
-
-public class NotFoundIDException extends Exception {
-	
-	public NotFoundIDException() {
-		super("존재하지 않는 아이디 입니다.");
-	}
-
-}
--- JSP_AdminLTE3/src/com/jsp/exception/NullpasswordException.java
+++
@@ -1,9 +0,0 @@
-package com.jsp.exception;
-
-public class NullpasswordException extends Exception {
-	
-	public NullpasswordException() {
-		super("패스워드 입력해줄래요?");
-	}
-
-}
