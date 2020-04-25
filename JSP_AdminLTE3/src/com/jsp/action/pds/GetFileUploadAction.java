69c1d70bcd09f0b923956f138f4fbee383ac9389
dldbfl <pangpang1861@naver.com>
2020-04-25 18:01:10 +0900

    토요아ㅣㄹ

--- JSP_AdminLTE3/src/com/jsp/action/pds/GetFileUploadAction.java
+++ JSP_AdminLTE3/src/com/jsp/action/pds/GetFileUploadAction.java
@@ -1,11 +1,9 @@
-package com.jsp.action.member;
+package com.jsp.action.pds;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.SQLException;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -16,26 +14,33 @@ import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 import com.jsp.action.Action;
-import com.jsp.dto.BoardVO;
-import com.jsp.request.SearchCriteria;
-import com.jsp.service.BoardService;
+import com.jsp.dto.AttachVO;
+import com.jsp.dto.MemberVO;
+import com.jsp.dto.PdsVO;
+import com.jsp.request.MemberRegistRequest;
+import com.jsp.service.PdsService;
 import com.jsp.utils.GetUploadPath;
 import com.jsp.utils.MakeFileName;
 import com.jsp.utils.MakeLogForException;
 
-public class MemberUploadPictureAction implements Action {
+public class GetFileUploadAction implements Action {
 	
 //	업로드 파일 환경 설정
 	private static final int MEMORY_THRESHOLD = 1024 * 500;             //
 	private static final int MAX_FILE_SIZE    = 1024 * 1024 * 1;        //
-	private static final int MAX_REQUEST_SIZE = 1024 * 1024 * 2;        //
+	private static final int MAX_REQUEST_SIZE = 1024 * 1024 * 5;        //
 
+	private PdsService pdsService;
+	public void setPdsService(PdsService pdsService) {
+		this.pdsService = pdsService;
+	}
+	
 	
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
-		String fileName = null;
+
+		PdsVO fileName = null;
 		try {
 			fileName = saveFile(request,response);
 		}  catch (Exception e) {
@@ -51,9 +56,11 @@ public class MemberUploadPictureAction implements Action {
 		
 		return null;
 	}
-	
-	private String saveFile(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, Exception {
 
+	private PdsVO saveFile(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, Exception {
+
+		PdsVO pdsVO = new PdsVO();
+		
 		//request 파잏 첨부 확인.
 		if(!ServletFileUpload.isMultipartContent(request)) {
 			 throw new Exception(); // throw Exception 처리
@@ -74,7 +81,7 @@ public class MemberUploadPictureAction implements Action {
 		//업로드 request 크기 적용.
 		upload.setSizeMax(MAX_REQUEST_SIZE);
 		
-		//실제 저장 경로를 설정.s
+		//실제 저장 경로를 설정. <---- 여기서 요일별로 나눠야해
 		String uploadPath=GetUploadPath.getUploadPath("pds.upload");
 		File file = new File(uploadPath);
 		if(!file.mkdirs()) {
@@ -84,15 +91,51 @@ public class MemberUploadPictureAction implements Action {
 		//request로부터 받는 파일을 추출해서 저장
 	
 		List<FileItem> formItems = upload.parseRequest(request);
-		String fileName = null;
+		
 		//파일 개수 확인
 		if(formItems != null && formItems.size() > 0) {
 			for(FileItem item : formItems) {//form items 반복해서 꺼내는 구문
 				if(!item.isFormField()) {//파일일 경우 해당-업로드된 파일 저장
-					//uuid+구분자+파일명
-					fileName = MakeFileName.toUUIDFileName(".jpg", "");
-					String filePath = uploadPath + File.separator + fileName;
-					File storeFile = new File(filePath);
+					
+					
+					//작업시작
+					List<AttachVO> attachList = null;
+												
+					String fileName = item.getFieldName(); //파라미터이름
+					String uploadsPath = uploadPath + File.separator + MakeFileName.toUUIDFileName(".txt", "");
+					String fileType = item.getContentType(); //파일타입
+					String attacher = item.getFieldName("writer");
+					
+					
+					
+					
+					
+					
+					for(int i=0; i<5; i++) {
+						
+						attachList.set(i, item);
+					}
+					
+					pdsVO.setAttachList(attachList);
+					
+					
+					//String fileName = item.getName(); //원본 파일명
+					
+				  	//File file = new File("C://Users/Administrator/Downloads/s/"+fileName); 
+					
+				
+				  	/*MemberRegistRequest memberReq = new MemberRegistRequest(id,pwd,authority,email,phone,picture,name);
+				       
+			        MemberVO member = memberReq.toMemberVO();
+				      
+				      	try {
+				      		memberService.regist(member);
+					*/
+					
+					
+					
+					
+					
 					
 					//local HDD에 저장.
 					item.write(storeFile);
@@ -114,16 +157,8 @@ public class MemberUploadPictureAction implements Action {
 		
 		
 		
-		return fileName;
+		return pdsVO;
 	}
 
+	
 }
-
-
-
-
-
-
-
-
-
--- JSP_AdminLTE3/src/com/jsp/action/pds/RegistPdsAction.java
+++ JSP_AdminLTE3/src/com/jsp/action/pds/RegistPdsAction.java
@@ -7,15 +7,23 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.Date;
+import java.util.List;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
 import com.jsp.action.Action;
+import com.jsp.dto.PdsVO;
 import com.jsp.service.PdsService;
 import com.jsp.utils.GetUploadPath;
+import com.jsp.utils.MakeFileName;
+import com.jsp.utils.MakeLogForException;
 
 public class RegistPdsAction implements Action{
 	
@@ -29,19 +37,19 @@ public class RegistPdsAction implements Action{
 	//	업로드 파일 환경 설정
 	private static final int MEMORY_THRESHOLD = 1024 * 500;             //
 	private static final int MAX_FILE_SIZE    = 1024 * 1024 * 1;        //
-	private static final int MAX_REQUEST_SIZE = 1024 * 1024 * 2;        //
+	private static final int MAX_REQUEST_SIZE = 1024 * 1024 * 5;        //
 
 	
-	
-	
+		
 	
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String url="null";
+
+		PdsVO pdsVO = new PdsVO();
 		
 		
-		int pno;
 		String title;
 		String content;
 		String writer;
@@ -51,40 +59,23 @@ public class RegistPdsAction implements Action{
 		
 		
 		
+		pdsService.regist(pdsVO);		
+
+		
+		
+		return url;
 		
 		
 		
 		
-		
-		
-		String fileName = multi.getParameter("picture");
-		String savedPath = GetUploadPath.getUploadPath("pds.upload");
-		
-		String filePath = savedPath+fileName;
-		
-		sendFile(request, response, filePath);
-		
-		
-		
-		
-		
-		
-		try {
-		
-		pdsService.regist(pds);
-		
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		response.setContentType("text/html;charset=utf-8");
+/*		response.setContentType("text/html;charset=utf-8");
 		PrintWriter out=response.getWriter();
 		out.println("<script>");
 		out.println("window.opener.location.href='list.do';window.close();");
 		out.println("</script>");
+*/		
 		
 		
-		return url;
 	}
 	
 	
@@ -93,44 +84,6 @@ public class RegistPdsAction implements Action{
 	
 	
 	
-	
-	
-	private void sendFile(HttpServletRequest request, HttpServletResponse response, String filePath) throws ServletException, IOException {
-		
-		// 보낼 파일 설정.
-		File downloadFile = new File(filePath);
-		FileInputStream inStream = new FileInputStream(downloadFile);
-		
-		ServletContext context = request.getSession().getServletContext();
-		
-		// 파일 포맷으로 MIME를 결정한다.
-		String mimeType = context.getMimeType(filePath);
-		if(mimeType == null) {
-			mimeType = "application/octet-stream";
-		}
-		System.out.println("MIME type: "+ mimeType);
-		
-		// response 수정.
-		response.setContentType(mimeType);
-		response.setContentLength((int)downloadFile.length());
-		
-		String headerKey = "Content-Disposition";
-		String headerValue = String.format("attachment; filename=\"%s\"", downloadFile.getName());
-		response.setHeader(headerKey, headerValue);
-		
-		// 파일 내보내기
-		OutputStream outputStream = response.getOutputStream();
-		byte[] buffer = new byte[4096];
-		int bytesRead = -1;
-		
-		while ((bytesRead = inStream.read(buffer)) != -1) {
-			outputStream.write(buffer,0,bytesRead);
-		}
-		
-		if(inStream != null ) inStream.close();
-		if(outputStream != null ) outputStream.close();
-		
-	}
 
 	
 	
